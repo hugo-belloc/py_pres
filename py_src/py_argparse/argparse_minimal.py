f497907df1ba1acbc461fbe20562018cb37c3a1f
Hugo Belloc <belloc.hugo@gmail.com>
2017-06-05 01:14:08 +0200

    Petite correction du code minimal

--- py_src/py_argparse/argparse_minimal.py
+++ py_src/py_argparse/argparse_minimal.py
@@ -4,8 +4,8 @@ import argparse
 def main(args=None):
     desc = "Exemple de parseur ligne de commande"
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("positional_arg")
-    parser.add_argument("-o", "--optional-arg")
+    parser.add_argument("positional_arg", help="Arg positionnel")
+    parser.add_argument("-o", "--optional-arg", help="Arg optionnel")
     args = parser.parse_args(args)
     print("positional_arg={}".format(args.positional_arg))
     print("optional_arg={}".format(args.optional_arg))
